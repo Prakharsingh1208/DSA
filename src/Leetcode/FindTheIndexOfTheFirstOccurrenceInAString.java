913fc433d72ecb8133e4c221a72b3fd4247b0613
Prakharsingh1208 <prakharsingh1208@gmail.com>
2025-12-29 09:49:11 +0530

    solved SearchInsertPosition leetcode problem

--- src/Leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/Leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,4 +1,26 @@
 package Leetcode;
 
 public class FindTheIndexOfTheFirstOccurrenceInAString {
+    public static void main(String...Args){
+        String haystack = "a";
+        String needle = "a";
+        System.out.println(strStr(haystack, needle));
+
+    }
+    static int strStr(String haystack, String needle){
+        int LengthOfNeedle = needle.length();
+        int LengthOfHaystack = haystack.length();
+        if(LengthOfNeedle==0 || LengthOfHaystack<LengthOfNeedle){
+            return -1;
+        }
+        for (int i=0; i<LengthOfHaystack-LengthOfNeedle+1;i++){
+            if(haystack.charAt(i)==needle.charAt(0)){
+                if(haystack.substring(i,i+LengthOfNeedle).equals(needle)){
+                    return i;
+                }
+            }
+
+        }
+        return -1;
+    }
 }
--- src/Leetcode/SearchInsertPosition.java
+++ src/Leetcode/SearchInsertPosition.java
@@ -1,4 +1,27 @@
 package Leetcode;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class SearchInsertPosition {
+    public static void main(String ...Args){
+        int[] nums={1,3,5,6,8};
+        int target=2;
+        System.out.println(searchInsert(nums,target));
+    }
+    static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start<=end){
+            int mid  = (start+end)/2;
+            if(nums[mid]==target){
+                return mid;
+            } else if (nums[mid]<target) {
+                start = mid+1;
+            }else {
+                end = mid-1;
+            }
+        }
+        return end+1;
+    }
 }
