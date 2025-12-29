78ea1ee33671b5223b6649d17dbf7a6169bdee7a
Prakharsingh1208 <prakharsingh1208@gmail.com>
2025-12-29 09:52:11 +0530

    Learned the implimentation of binary search in java

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,22 @@
 public class BinarySearch {
+        public static void main(String ...Args){
+            int[] nums={1,3,5,6,8};
+            int target=5;
+            System.out.println(BinarySearch(nums,target));
+        }
+        static int BinarySearch(int[] nums, int target) {
+            int start = 0;
+            int end = nums.length - 1;
+            while(start<=end){
+                int mid  = (start+end)/2;
+                if(nums[mid]==target){
+                    return mid;
+                } else if (nums[mid]<target) {
+                    start = mid+1;
+                }else {
+                    end = mid-1;
+                }
+            }
+            return -1;
+        }
 }
