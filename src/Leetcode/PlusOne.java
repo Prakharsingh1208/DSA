fc4dfc93d2166584a0dbd70bb47e70a8489977ff
Prakharsingh1208 <prakharsingh1208@gmail.com>
2025-12-31 11:30:14 +0530

    solved PlusOne leetcode problems

---
+++ src/Leetcode/PlusOne.java
@@ -0,0 +1,23 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String... Args){
+        int[] digits= {9};
+        int[] a = plusOne(digits);
+        System.out.println(Arrays.toString(a));
+    }
+    static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
