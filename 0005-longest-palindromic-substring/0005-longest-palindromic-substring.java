067660a6b21d39f62df25bcaff4e23c7985c102e
Prakhar Singh <prakharsingh1208@gmail.com>
2026-01-08 20:19:18 +0530

    Time: 415 ms (17.08%), Space: 48.6 MB (6.72%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,33 +1,50 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) return s;
-
         String lp1 = "";
-
-        for (int i = 0; i < s.length(); i++) {
-
-            // EVEN palindrome
-            int l = i, r = i + 1;
-            String dummy = "";
-            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                dummy = s.charAt(l) + dummy + s.charAt(r);
-                l--;
-                r++;
-            }
-            if (dummy.length() > lp1.length()) lp1 = dummy;
-
-            // ODD palindrome
-            l = i - 1;
-            r = i + 1;
-            String dummy2 = Character.toString(s.charAt(i));
-            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                dummy2 = s.charAt(l) + dummy2 + s.charAt(r);
-                l--;
-                r++;
-            }
-            if (dummy2.length() > lp1.length()) lp1 = dummy2;
+        if(s.length()==1){
+            return s;
         }
-
+        for(int i=0; i<s.length()-1;i++){
+            int l=i;
+            int r=i+1;
+            int lenP=0;
+            String dummy ="";
+            String dummy2 ="";
+            if(s.charAt(i)==s.charAt(i+1)){
+                dummy = Character.toString(s.charAt(i))+Character.toString(s.charAt(i+1));
+                while(l-1>=0 && r+1<s.length()){
+                    String a=Character.toString(s.charAt(l-1));
+                    String b=Character.toString(s.charAt(r+1));
+                    if(a.equals(b)){
+                        l--;
+                        r++;
+                        dummy = a+dummy+b;
+                    }else{
+                        break;
+                    }
+                }
+            }
+            l=i-1;
+            r=i+1;
+            dummy2 =Character.toString(s.charAt(i));
+            while(l>=0 && r<s.length()){
+                String a=Character.toString(s.charAt(l));
+                String b=Character.toString(s.charAt(r));
+                if(a.equals(b)){
+                    l--;
+                    r++;
+                    dummy2 = a+dummy2+b;
+                }else{
+                    break;
+                }
+            }
+            if(dummy.length()>lp1.length()){
+                lp1=dummy;
+            }
+            if(dummy2.length()>lp1.length()){
+                lp1=dummy2;
+            }
+        }
         return lp1;
     }
 }
