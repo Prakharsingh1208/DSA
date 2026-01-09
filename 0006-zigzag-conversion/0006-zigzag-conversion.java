e549554930a8606c5e344f6caf4992afb6e4e41f
Prakhar Singh <prakharsingh1208@gmail.com>
2026-01-09 07:46:47 +0530

    Time: 12 ms (18.29%), Space: 47.6 MB (9.76%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1){ return s;}
+        List<List<String>> row = new ArrayList<>();
+        int r = numRows;
+        while(r!=0){
+            row.add(new ArrayList<String>());
+            r--;
+        }
+
+        boolean con = true;
+        int rowCount=0;
+        for(int i=0;i<s.length();i++){
+            if(con){
+                row.get(rowCount).add(Character.toString(s.charAt(i)));
+                if(rowCount==numRows-1){
+                    con=false;
+                    rowCount = rowCount-1;
+                }else{
+                    rowCount++;
+                }
+                continue;
+            }
+            else if(!con){
+                row.get(rowCount).add(Character.toString(s.charAt(i)));
+                if(rowCount==0){
+                    con=true;
+                    rowCount=1;
+                }else{
+                    rowCount--;
+                }
+
+            }
+        }
+
+        String result = "";
+        for(int i=0;i<row.size();i++){
+            for(int j=0;j<row.get(i).size();j++){
+                result = result+row.get(i).get(j);
+            }
+        }
+        return result;
+        
+    }
+}
