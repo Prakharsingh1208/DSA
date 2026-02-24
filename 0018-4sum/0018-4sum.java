9b4a30fb8b78a61d3b76f34870ec18f250154c16
Prakhar Singh <prakharsingh1208@gmail.com>
2026-02-24 07:50:19 +0530

    Time: 18 ms (88.68%), Space: 46 MB (47.11%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,46 +1,49 @@
 class Solution {
+    //a>0, b, c , d<n. 
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
         int n = nums.length;
-
-        if (n < 4) return result;
 
         Arrays.sort(nums);
 
-        for (int i = 0; i < n - 3; i++) {
-            // skip duplicates for i
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        List<List<Integer>> result = new ArrayList<>();
 
-            for (int j = i + 1; j < n - 2; j++) {
-                // skip duplicates for j
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+        for(int i=0;i<n-3;i++){
+            if(i>0 && nums[i]== nums[i-1]){
+                continue;
+            }
 
-                int left = j + 1;
-                int right = n - 1;
+            for(int j=i+1; j<n-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
 
-                while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                int left=j+1;
+                int right=n-1;
 
-                    if (sum == target) {
-                        result.add(Arrays.asList(
-                                nums[i], nums[j], nums[left], nums[right]
-                        ));
+                while(left<right){
 
+                    long sum = (long) nums[i]+nums[j]+nums[left]+nums[right];
+                    
+                    if(sum < target){
                         left++;
+                    }else if(sum>target){
                         right--;
+                    }else{
+                        result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
 
-                        // skip duplicates for left
-                        while (left < right && nums[left] == nums[left - 1]) left++;
-                        // skip duplicates for right
-                        while (left < right && nums[right] == nums[right + 1]) right--;
-
-                    } else if (sum < target) {
+                        while(left<right && nums[left] ==nums[left+1]) left++;
+                        while(left<right && nums[right] ==nums[right-1]) right--;
                         left++;
-                    } else {
                         right--;
                     }
+
+
                 }
+
+
             }
+
+
         }
         return result;
     }
