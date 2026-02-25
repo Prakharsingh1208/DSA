418e5380b57269738a2fbf33258a196ab0e5697d
Prakhar Singh <prakharsingh1208@gmail.com>
2026-02-25 17:32:10 +0530

    Time: 0 ms (100%), Space: 42.2 MB (29.56%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,17 +1,14 @@
 class Solution {
     public int numTrees(int n) {
-        // dp[i] = number of unique BSTs with i nodes
         int[] dp = new int[n + 1];
         
-        // Base cases
-        dp[0] = 1; // empty tree
-        dp[1] = 1; // single node
+        dp[0] = 1; 
+        dp[1] = 1; 
         
-        // Fill dp bottom-up
         for (int nodes = 2; nodes <= n; nodes++) {
             for (int root = 1; root <= nodes; root++) {
-                int leftCount  = dp[root - 1];      // nodes to the left of root
-                int rightCount = dp[nodes - root];   // nodes to the right of root
+                int leftCount  = dp[root - 1];      
+                int rightCount = dp[nodes - root];   
                 dp[nodes] += leftCount * rightCount;
             }
         }
